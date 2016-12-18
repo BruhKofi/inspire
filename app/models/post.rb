c00d86d9d907efe3cc2da595b378d1aa549532f1
Tynnee <victorasantekyei@yahoo.co.uk>
2016-12-18 01:56:25 +0100

    try fix messed up categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,7 @@
 class Post < ApplicationRecord
   acts_as_votable
   belongs_to :user
-  belongs_to :category
   has_many :comments
   has_attached_file :image, styles: { medium: "750x500>", small: "350x250>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-  validates :category, presence: true
 end
